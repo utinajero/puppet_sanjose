0b8bd2dd00849c22584927729c183515ef227de9
Ulysses Tinajero <utinajero07@gmail.com>
2014-07-18 10:37:27 -0700

    comment hiera absent

--- windows_desktop_shortcuts/manifests/params.pp
+++ windows_desktop_shortcuts/manifests/params.pp
@@ -1,6 +1,6 @@
 class windows_desktop_shortcuts::params {
 
-  $absent_shortcuts  = hiera('win_absent_shortcuts')
+  #$absent_shortcuts  = hiera('win_absent_shortcuts')
   $present_shortcuts = hiera('win_present_shortcuts')
   $present_content   = hiera('win_present_content')
   
