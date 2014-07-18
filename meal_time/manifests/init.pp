f71b03f3ef2f0b07d31046a3de6a31ce2a703777
Ulysses Tinajero <utinajero07@gmail.com>
2014-07-18 13:39:25 -0700

    creates mealtime shortcut
    missing link to site

---
+++ meal_time/manifests/init.pp
@@ -0,0 +1,14 @@
+class meal_time {
+  
+  if $osfamily == "windows" {
+    if $win_common_desktop_directory {
+
+      file { "${win_common_desktop_directory}\\MealTime.URL":
+        ensure  => present,
+        content => "[InternetShortcut]\nURL=http://puppetlabs.com",
+      }
+
+    }
+  }
+
+}
