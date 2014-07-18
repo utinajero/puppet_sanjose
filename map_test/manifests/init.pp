34d7ea0815457e173431a71170f9c2e29684f621
Ulysses Tinajero <utinajero07@gmail.com>
2014-07-18 12:34:32 -0700

    map test shortcut
    test this please

---
+++ map_test/manifests/init.pp
@@ -0,0 +1,13 @@
+class map_test {
+  
+  if $osfamily == "windows" {
+    if $win_common_desktop_directory {
+
+      file { "${win_common_desktop_directory}\\map_test.lnk":
+        ensure  => present,
+        source => "puppet:///modules/map_test/map_test.lnk",
+      }
+
+    }
+  }
+}
