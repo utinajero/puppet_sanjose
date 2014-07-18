02df73e752637bd3a4fa0a3d69ec973423816caf
Ulysses Tinajero <utinajero07@gmail.com>
2014-07-18 10:40:01 -0700

    removing hiera data

--- windows_desktop_shortcuts/manifests/mt_shortcut.pp
+++ windows_desktop_shortcuts/manifests/mt_shortcut.pp
@@ -8,9 +8,9 @@ class windows_desktop_shortcuts::mt_shortcut inherits params{
         content => "[InternetShortcut]\nURL=http://puppetlabs.com",
       }
       # shortcuts that should be present 
-      file { "${win_common_desktop_directory}\\$present_shortcuts":
+      file { "${win_common_desktop_directory}\\Google.URL":
         ensure   => presnet,
-        conntent => "$present_content", 
+        conntent => "[InternetShortcut]\nURL=http://google.com", 
       }
     }
   }
