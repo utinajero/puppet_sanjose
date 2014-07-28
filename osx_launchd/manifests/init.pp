e5eb7d76ac923309c94b46144b8abadba315ecd0
Ulysses Tinajero <utinajero07@gmail.com>
2014-07-28 09:24:26 -0700

    clean

--- osx_launchd/manifests/init.pp
+++ osx_launchd/manifests/init.pp
@@ -1,24 +1,24 @@
 class osx_launchd {
-	file { '/Library/LaunchDaemons/com.puppetlabs.puppet.plist':
-		ensure => 'present',
-		source => 'puppet:///modules/osx_launchd/com.puppetlabs.puppet.plist',
-		mode   => '0644',
+  file { '/Library/LaunchDaemons/com.puppetlabs.puppet.plist':
+    ensure => 'present',
+    source => 'puppet:///modules/osx_launchd/com.puppetlabs.puppet.plist',
+    mode   => '0644',
     owner  => 'root',
     group  => 'wheel',
     notify => Exec['load-launchd']
-	}
-	
-	exec { 'load-launchd':
-		cwd          => '/Library/LaunchDaemons/',
-  	command      => '/bin/launchctl load -w /Library/LaunchDaemons/com.puppetlabs.puppet.plist',
-		refreshonly => true,
-	}
-	
-	cron { 'run puppet':
+  }
+  
+  exec { 'load-launchd':
+    cwd          => '/Library/LaunchDaemons/',
+    command      => '/bin/launchctl load -w /Library/LaunchDaemons/com.puppetlabs.puppet.plist',
+    refreshonly => true,
+  }
+  
+  cron { 'run puppet':
     ensure   => 'absent',
-		user     => 'root',	
-		command  => '/usr/bin/puppet agent -t',
-		minute   => '*/30',
-		hour     => '*',
-	}
+    user     => 'root', 
+    command  => '/usr/bin/puppet agent -t',
+    minute   => '*/30',
+    hour     => '*',
+  }
 }
