15d0aa2b2e1720cc226c445d3a48f9656be28e8b
Ulysses Tinajero <utinajero07@gmail.com>
2014-08-11 15:18:47 -0700

    updated params and url link

--- meal_time/manifests/init.pp
+++ meal_time/manifests/init.pp
@@ -1,11 +1,11 @@
-class meal_time {
+class meal_time inherits params {
   
   if $osfamily == "windows" {
     if $win_common_desktop_directory {
 
       file { "${win_common_desktop_directory}\\MealTime.URL":
         ensure  => present,
-        content => "[InternetShortcut]\nURL=http://mealtime.com",
+        content => "[InternetShortcut]\nURL=$meal_time_url",
       }
 
     }
---
+++ meal_time/manifests/params.pp
@@ -0,0 +1,5 @@
+class meal_times::params {
+  
+  $meal_time_url = hiera('mt_url')
+  
+}
