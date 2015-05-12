ef946df1a87a3bba5a6ef1bf87be411aa0941266
Ulysses Tinajero <utinajero07@gmail.com>
2015-05-12 08:30:14 -0700

    remove

--- chocolatey_apps/manifests/installs.pp
+++ chocolatey_apps/manifests/installs.pp
@@ -4,19 +4,6 @@ class chocolatey_apps::installs inherits params {
     package { "$win_pkgs":
       ensure   => $ensure,
       provider => $provider,
-      #require => File['install_chocolatey.bat'],
     }
-    
-    # file { 'install_chocolatey.bat':
-    #   path   => 'C:\install_cocolatey.bat',
-    #   ensure => present,
-    #   source => 'puppet:///modules/chocolatey_apps/files/install_chocolatey.bat',
-    # }
-    #
-    # exec { 'C:\install_cocolatey.bat':
-    #   onlyif  => 'C:\install_cocolatey.bat',
-    #   require => File['install_chocolatey.bat'],
-    #   creates =>  "C:\puppet_installed_chocolatey.txt",
-    # }
   }
 }
