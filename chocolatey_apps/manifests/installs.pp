25690832e4bb6bb3c7ea75707167c74b62d2dd9c
Ulysses Tinajero <utinajero07@gmail.com>
2015-05-12 08:32:11 -0700

    update

--- chocolatey_apps/manifests/installs.pp
+++ chocolatey_apps/manifests/installs.pp
@@ -1,7 +1,7 @@
 class chocolatey_apps::installs inherits params {
   
   if $::kernel == windows {
-    package { "$win_pkgs":
+    package { $win_pkgs:
       ensure   => $ensure,
       provider => $provider,
     }
